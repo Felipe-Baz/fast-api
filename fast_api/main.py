e9138629c6c1599f3eac5974995c9039a95539b3
Felipe Baz <fbazmitsuishi@gmail.com>
2023-07-13 12:16:54 -0300

    feat: Middlewares

--- fast_api/main.py
+++ fast_api/main.py
@@ -1,7 +1,8 @@
+import time
 from typing import Annotated, Union
 
-from fastapi import FastAPI
-from fastapi.params import Body, Cookie, Path, Query
+from fastapi import FastAPI, Request
+from fastapi.params import Body, Cookie, Depends, Path, Query
 from pydantic import BaseModel, Field
 
 app = FastAPI()
@@ -18,9 +19,17 @@ class Item(BaseModel):
         default=False, title="The price of the item", gt=0
     )
 
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
 
 @app.get("/")
 def read_root():
+    time.sleep(1)
     return {"Hello": "World"}
 
 
@@ -70,3 +79,10 @@ def update_item(
 @app.get("/cookies")
 def cookieGet(ads_id: Annotated[str | None, Cookie()] = None):
     return {"cookies": ads_id}
+
+async def common_parameters(q: str | None = None, skip: int = 0, limit: int = 100):
+    return {"q": q, "skip": skip, "limit": limit}
+
+@app.get("/commons")
+def request_with_commons_parameters(commons: Annotated[dict, Depends(common_parameters)]):
+    return commons
